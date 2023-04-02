23b2e67dc292e17c94a7a364b4f09ea3f36fbd60
María Cecilia Sangineto Ruibal <ceciliasangi@gmail.com>
2023-04-02 16:12:47 -0300

    Agregando el video

--- TP.js
+++ TP.js
@@ -5,3 +5,9 @@ botonHistoria.addEventListener('click', () => {
 botonDeIrAHistoria.addEventListener('click', () => {
     window.location.href="./TP-Historia.html";
 })
+
+let video = document.querySelector("video")
+
+const pausarVideo =()=> {
+    video.pause()
+}
