5c0d3e67b27eea2160ba2862f3599cc5aedb7521
María Cecilia Sangineto Ruibal <ceciliasangi@gmail.com>
2023-05-01 15:10:27 -0300

    Arreglando algunos botones

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ botonJuego.addEventListener('click', () => {
     window.location.href="./juego.html";
 })
 
+botonDeIrAJuego.addEventListener('click', () => {
+    window.location.href="./juego.html";
+})
+
+botonDeIrAHistoria.addEventListener('click', () => {
+    window.location.href="./historia.html";
+})
 
 logo.addEventListener('click', () => {
     window.location.href="./index.html";
