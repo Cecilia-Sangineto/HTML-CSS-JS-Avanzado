bbf84398514efe63cb760e18e7e70f9d6d4977fd
María Cecilia Sangineto Ruibal <ceciliasangi@gmail.com>
2023-05-01 14:47:21 -0300

    Mejorando puzzle

--- juego.js
+++ juego.js
@@ -55,19 +55,47 @@ puzzle.addEventListener('dragleave', e => {
     e.target.classList.remove('hover');
 });
 
+let numeros = []
+let ids = []
+
 puzzle.addEventListener('drop', e => {
     e.target.classList.remove('hover');
 
     const id = e.dataTransfer.getData('id');
     const numero = id.split('-')[1];
 
-    if (e.target.dataset.id === numero) {
-        e.target.appendChild(document.getElementById(id));
+    // if (e.target.dataset.id === numero) {
+        e.target.appendChild(document.getElementById(id)); 
 
+        ids.push(e.target.dataset.id)
+        numeros.push(id.split('-')[1])
         terminado--;
-
+    
         if (terminado === 0) {
-            document.body.classList.add('ganaste');
-        }
+            if( sonIguales(ids,numeros) ){
+                document.body.classList.add('ganaste');
+                mensaje.innerHTML = "Ganaste!"
+            }
+            else {
+                document.body.classList.add('perdiste');
+                mensaje.innerHTML = "Perdiste, intentalo de nuevo!"
+            }
     }
 });
+
+function sonIguales(arreglo1, arreglo2) {
+    // Si los arreglos tienen diferente longitud, no son iguales
+    if (arreglo1.length !== arreglo2.length) {
+      return false;
+    }
+    
+    // Comparamos los elementos de cada arreglo
+    for (let i = 0; i < arreglo1.length; i++) {
+      if (arreglo1[i] !== arreglo2[i]) {
+        return false;
+      }
+    }
+    
+    // Si llegamos hasta aquí, los arreglos son iguales
+    return true;
+  }
